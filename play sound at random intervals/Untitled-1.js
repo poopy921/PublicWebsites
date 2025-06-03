4e11faa16c44e87ba215ba446fc0e79185bab676
Jerico <jbote5@uic.edu>
2025-06-03 17:30:16 -0500

    random

---
+++ play sound at random intervals/Untitled-1.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param popop I love this var (can be anything)!
+ * @returns it returns 0 if no errors occur in it.
+ */
+function main(popop) {
+    document.write(popop);
+    return 0;
+}
+main()
